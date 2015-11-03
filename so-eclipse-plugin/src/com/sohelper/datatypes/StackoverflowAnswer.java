1651be79ec7addc3786350cd469993b509721510
Hiren Savalia <hiren_879@yahoo.com>
2015-11-03 22:48:42 +0530

    committing changes for showing only accepted answers.

--- so-eclipse-plugin/src/com/sohelper/datatypes/StackoverflowAnswer.java
+++ so-eclipse-plugin/src/com/sohelper/datatypes/StackoverflowAnswer.java
@@ -16,9 +16,21 @@ public class StackoverflowAnswer {
 	private String voteCount;
 	private String userUrl;
 	
+	private Boolean isAcceptedAnswer;
+	
+	// String Constants
+	private final String ACCEPTED_ANSWER = "acceptedAnswer";
+	private final String ITEM_PROP = "itemprop";
+	
 	public StackoverflowAnswer(Element element) {
 		try {
 			this.element = element;
+			String acceptedAnswerText = element.getElementsByAttributeValue(ITEM_PROP, ACCEPTED_ANSWER).attr(ITEM_PROP).toString();
+			if(acceptedAnswerText != null && !acceptedAnswerText.isEmpty() && ACCEPTED_ANSWER.equals(acceptedAnswerText)){
+				this.isAcceptedAnswer = true;
+			}else{
+				this.isAcceptedAnswer = false;
+			}
 			Element userElement = element.select("table.fw").select("div.user-info").last().select("a").last();
 			this.user = userElement.text();
 			this.userUrl = "stackoverflow.com" + userElement.attr("href");
@@ -77,5 +89,13 @@ public class StackoverflowAnswer {
 
 	public void setUserUrl(String userUrl) {
 		this.userUrl = userUrl;
+	}
+	
+	public Boolean getIsAcceptedAnswer() {
+		return isAcceptedAnswer;
+	}
+
+	public void setIsAcceptedAnswer(Boolean isAcceptedAnswer) {
+		this.isAcceptedAnswer = isAcceptedAnswer;
 	}
 }
